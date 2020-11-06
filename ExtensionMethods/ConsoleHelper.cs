e019b26be16229448c3f288d27549af3c17f8df9
j miller <36433882+CurbYourStrangeness@users.noreply.github.com>
2020-11-06 17:50:06 -0600

    Initial Commit

---
+++ ExtensionMethods/ConsoleHelper.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExtensionMethods
+{
+    static class ConsoleHelper
+    {
+        public static string RequestString(this string message)
+        {
+            string outp = "";           
+
+            while (string.IsNullOrEmpty(outp))
+            {
+                Console.WriteLine(message);
+                outp = Console.ReadLine();
+            }
+
+            return outp;
+        }
+
+        public static int RequestInt(this string message, int minVal, int maxVal)
+        {
+            return message.RequestInt(true, minVal, maxVal);
+        }
+
+        public static int RequestInt(this string message)
+        {
+            return message.RequestInt(false);
+        }
+
+        private static int RequestInt(this string message, bool useMinMax, int minVal = 0, int maxVal = 0)
+        {
+            int i = 0;
+            bool isValidInt = false;
+            bool isInValidRange = true;
+
+            while (!isValidInt || !isInValidRange)
+            {
+                Console.WriteLine(message);
+                isValidInt = int.TryParse(Console.ReadLine(), out i);
+
+                if (useMinMax)
+                {
+                    isInValidRange = (i >= minVal && i <= maxVal);
+                    //{
+                    //    isInValidRange = true;
+                    //}
+                    //else
+                    //{
+                    //    isInValidRange = false;
+                    //}
+                }
+            
+            }
+            if (i < 0)
+            {
+                return 0;
+            }
+            else
+            {
+                return i;
+            }
+        }
+    }
+}
---
+++ ExtensionMethods/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExtensionMethods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PersonModel pers = new PersonModel();
+
+            pers.fName = "What is your first name?".RequestString();
+
+            pers.lName = "What is your last name?".RequestString();
+
+            pers.Age = ConsoleHelper.RequestInt("What is your age?", 0, 999999999);
+
+            Console.WriteLine($"{pers.fName}, {pers.lName}, {pers.Age}");
+
+            Console.ReadLine();
+        }
+    }
+
+    public class PersonModel
+    {
+        public string fName { get; set; }
+
+        public string lName { get; set; }
+
+        public int Age { get; set; }
+
+        public int ChildrenNum { get; set; }
+
+    }
+}
